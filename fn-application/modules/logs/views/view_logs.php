50c098b61e11b49f208e8444a259a9b41f02feeb
Junry Solloso <junry.s.solloso@gmail.com>
2021-04-07 14:57:08 +0800

    Updated log view

--- fn-application/modules/logs/views/view_logs.php
+++ fn-application/modules/logs/views/view_logs.php
@@ -6,7 +6,7 @@
         <th><i class="mdi mdi-account-outline icon-sm align-self-center text-primary mr-3"></i>Username</th>
         <th><i class="mdi mdi-calendar-outline icon-sm align-self-center text-success mr-3"></i>Date</th>
         <th><i class="mdi mdi-clock-outline icon-sm align-self-center text-info mr-3"></i>Time</th>
-        <th><i class="mdi mdi-file-alert-outline icon-sm align-self-center text-warning mr-3"></i>Activity</th>
+        <th><i class="mdi mdi-file-alert-outline icon-sm align-self-center text-danger mr-3"></i>Activity</th>
       </tr>
     </thead>
     <tbody>
@@ -38,7 +38,7 @@
           </td>
           <td>
             <div class="media">
-              <i class="mdi mdi-file-alert-outline icon-sm align-self-center text-warning mr-3"></i>
+              <i class="mdi mdi-file-alert-outline icon-sm align-self-center text-danger mr-3"></i>
               <div class="media-body my-auto">
                 <p class="mb-0"><?php echo ucfirst( $row->log_task ); ?></p>
               </div>
